51d8ed2d59a6f4b7667074952f3952b9b0066888
EnriqueMendozaPrrfect <lionsixeight@gmail.com>
2023-06-05 23:50:56 -0600

    Add tutorial 1 and 2

---
+++ 1-hello-world/hello_world.rb
@@ -0,0 +1,16 @@
+puts "Hello world"
+
+x = "hello world"
+puts x
+
+y = 5
+puts y
+
+y = 6
+puts y
+
+y = "asb"
+puts y
+
+is_valid = false
+puts is_valid
---
+++ 2-aritmetica/aritmetica.rb
@@ -0,0 +1,25 @@
+# () Parentesis
+# / y * División y multiplicación
+# + y -	Suma y resta
+# La forma en la que se resuelven es de izquierda a derecha
+
+suma = 5 + 6
+puts('el valor de la suma es:', suma)
+
+resta = 5 - 2
+puts('el valor de la resta es:', resta)
+
+multiplicacion = 3 * 4
+puts('el valor de la multiplicacion es:', multiplicacion)
+
+divicion = 12 % 3
+puts('el valor de la residuo es:', divicion)
+
+parentesis = (5 + 3) * 2
+puts('el valor de la parentesis es:', parentesis)
+
+# Ejercicios
+# Calcula el área de un cuadrado de 8 e imprime el resultado
+# Calcula el área de un triangulo de base 5 y altura 7 e imprime el resultado
+# Calcula el área de un trapesio de lado superior es de 5 y el lado inferior 7 
+# con altura de 4 e imprime el resultado.
